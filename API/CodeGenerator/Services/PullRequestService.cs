5919f2b247cc4e480e27e9a3dcf166a16f321223
pravinpatidar19 <pravin.patidar@irissoftware.com>
2025-04-02 14:10:43 +0530

    Update PullRequestService.cs
    test code

--- API/CodeGenerator/Services/PullRequestService.cs
+++ API/CodeGenerator/Services/PullRequestService.cs
@@ -142,6 +142,15 @@ namespace CodeGenerator.Services
             var response = await _httpClient.SendAsync(request);
             return response.IsSuccessStatusCode;
         }
-
+        
+         private void TestFunciont()
+         {
+             int[] numbers = new int[6] { 1, 2, 3, 4, 5, 6 };
+             for(int i = 0; i <= numbers.Length; i++)
+             {
+                 Console.WriteLine(numbers[i]);
+             }
+         }
+         
     }
 }
